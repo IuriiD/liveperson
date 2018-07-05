b6e1983ee626a62f00c5312b4d868bc7efc107f9
Iurii Dziuban <iurii.dziuban@gmail.com>
2018-07-05 17:49:44 +0300

    Anti 143 Heroku killing

--- App.js
+++ App.js
@@ -13,3 +13,10 @@ const keys = require('./keys');
 
 const agent = new agentBot(keys.lpAccountId, keys.lpAgentLogin, keys.lpAgentPassword);
 agent.start();
+
+let reqTimer = setTimeout(function wakeUp() {
+    request("https://liveperson.herokuapp.com", function() {
+        console.log("WAKE UP DYNO");
+    });
+    return reqTimer = setTimeout(wakeUp, 1200000);
+}, 1200000);
