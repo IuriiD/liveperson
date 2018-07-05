e2f3b153e10c70222e1b4faa2669bed88196310c
Iurii Dziuban <iurii.dziuban@gmail.com>
2018-07-05 16:19:22 +0300

    Changed Agent

--- keys.js
+++ keys.js
@@ -1,7 +1,7 @@
 const APIAITOKEN = "c2a8b983845543e2b0d54018ad01d2d1";
 const lpAccountId = "26004890";
-const lpAgentLogin = "testbot";
-const lpAgentPassword = "testbot111";
+const lpAgentLogin = "echobot";
+const lpAgentPassword = "echobot111";
 
 module.exports = {
     APIAITOKEN,
